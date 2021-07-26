c209c580797c83dc0f248e40463abc17fc7e2298
Daniel A-Sereboo <dsereboo@gmail.com>
2021-07-26 22:54:35 +0000

    Hover styles for sidebar nav items added

--- src/Components/NavContent.jsx
+++ src/Components/NavContent.jsx
@@ -34,7 +34,7 @@ const NavContent=(props)=>{
               <NavLink to="/" className="icon-side-spacing">
                 <svg width="24px" height="24px" viewBox="0 0 24 24">
                   <path
-                    fill="#f6f6f6"
+                    fill="rgba(255,255,255,.8)"
                     d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"
                   />
                 </svg>
